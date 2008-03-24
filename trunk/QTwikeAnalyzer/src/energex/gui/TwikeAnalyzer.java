0780df24c6307c87fa33076df6f6a9ce5f5b50cb
markus.walser <markus.walser@342c7f40-b674-6368-cd24-27efce68873d>
2008-03-24 20:18:44 +0000

    Switched to Qt Jambi 4.4 preview 1. And added a cool QtToolbar with QActions.

--- trunk/QTwikeAnalyzer/src/energex/gui/TwikeAnalyzer.java
+++ trunk/QTwikeAnalyzer/src/energex/gui/TwikeAnalyzer.java
@@ -1,3 +1,22 @@
+/***************************************************************************
+ *   Copyright (C) 2008 by Markus Walser                                   *
+ *   markus.walser@gmail.com                                               *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
 package energex.gui;
 
 import com.trolltech.qt.core.QDataStream;
@@ -5,12 +24,15 @@ import com.trolltech.qt.core.QFile;
 import com.trolltech.qt.core.QIODevice;
 import com.trolltech.qt.gui.*;
 
+import energex.communication.TwikePort;
 import energex.protocol.Decoder;
 
 public class TwikeAnalyzer extends QMainWindow{
 
     Ui_TwikeAnalyzerClass ui = new Ui_TwikeAnalyzerClass();
     QDataStream binaryStream;
+    
+    TwikePort port = new TwikePort("/dev/ttyS0");
     
     public static void main(String[] args) {
         QApplication.initialize(args);
@@ -21,14 +43,58 @@ public class TwikeAnalyzer extends QMainWindow{
     
     public TwikeAnalyzer(){
         ui.setupUi(this);
+        QStyle style = QStyleFactory.create("Plastique");
+        QApplication.setStyle(style);
     }
     
-    public TwikeAnalyzer(QWidget parent){
+	public TwikeAnalyzer(QWidget parent){
     	super(parent);
         ui.setupUi(this);
     }
+	
+	public void on_actionRecord_triggered() {
+    	try {
+			port.open();
+			ui.actionRecord.setEnabled(false);
+			ui.actionPlay.setEnabled(false);
+			ui.actionPause.setEnabled(true);
+			ui.actionStop.setEnabled(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+    }
+
+    public void on_actionPause_triggered() {
+    	try {
+			ui.actionPlay.setEnabled(true);
+			ui.actionPause.setEnabled(false);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+    }
+	
+    public void on_actionPlay_triggered() {
+    	try {
+			ui.actionPlay.setEnabled(false);
+			ui.actionPause.setEnabled(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+    }
     
-    public void on_openButton_clicked() {
+    public void on_actionStop_triggered() {
+    	try {
+			port.close();
+			ui.actionRecord.setEnabled(true);
+			ui.actionPlay.setEnabled(false);
+			ui.actionPause.setEnabled(false);
+			ui.actionStop.setEnabled(false);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+    }
+    
+    public void on_actionOpen_triggered() {
     	String fileName = null;
     	fileName = QFileDialog.getOpenFileName(this, tr("Open Image"), "/home/markus/" );
 //    	fileName = QFileDialog.getOpenFileName(this, tr("Open log file"), "~/", tr("Log Files (*.log)"), null);
